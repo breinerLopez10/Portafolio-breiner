1c67b44aff86440f1096feeeb98572d0bc87ba78
RAYWAR23 <breynerlopex234@gmail.com>
2023-02-03 08:34:46 -0500

    jhy

---
+++ Javascript/codigo.js
@@ -0,0 +1,23 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+
+  if (navMenu.classList.contains("nav-menu_visible")) {
+    navToggle.setAttribute("aria-label", "Cerrar menú");
+  } else {
+    navToggle.setAttribute("aria-label", "Abrir menú");
+  }
+});
+
+
+//slider
+const switchToggle = document.getElementById("switch"),
+    switchToggleImage = document.getElementById("switchToggleImage"),
+    container = document.getElementById("container");
+
+
+    
+
+
